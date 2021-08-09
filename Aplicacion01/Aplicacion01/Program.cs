9a777f74a3bf446ec93a5400baade5868ac316bd
Dulce Glenda Nineth Ortiz Gutiérrez <201800049@colsanjose.edu.gt>
2021-08-09 12:40:23 -0600

    corchetes

---
+++ Aplicacion01/Aplicacion01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aplicacion01
+{
+
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+            {
+
+                char op = 'n';
+                int opcion = 0;
+
+
+
+
+                StreamWriter arE;
+
+                while (op != 's')
+                {
+
+                    Console.WriteLine("1.- Nombre\n2.- Salir");
+                    Console.Write("Opcion: ");
+                    opcion = int.Parse(Console.ReadLine());
+
+
+
+                    if (opcion == 1)
+                    {
+
+                        Console.Write("Ingrese el nombre del archivo: ");
+                        arE = File.AppendText(Console.ReadLine() + ".txt");
+                        arE.Close();
+
+                    }
+
+                    else if (opcion == 2)
+
+                    {
+
+
+                        Console.Write("Desea Salir[s/n]");
+                        op = char.Parse(Console.ReadLine());
+                    }
+                }
+            
+                
+
+                }
+
+            }
+        }
+
+    }
+ 
+       
